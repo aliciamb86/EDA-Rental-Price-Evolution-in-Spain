cf17607c19ebb52b948c06b455a4c2c2fa02422c
aliciamb86 <alicia.mb86@gmail.com>
2024-03-12 23:04:49 +0100

    12/03/24 23:04

--- utils/funciones.py
+++ utils/funciones.py
@@ -10,13 +10,13 @@ def injuve(t1,t2,t3,t4):
     lista_df = [t1,t2,t3,t4]
     for archivo in lista_df:
         if '1T' in archivo:
-            trimestre1 = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],0:2]
+            trimestre1 = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],0:2]
         elif '2T' in archivo:
-            trimestre2 = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],1:2]
+            trimestre2 = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],1:2]
         elif '3T' in archivo:
-            trimestre3 = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],1:2]
+            trimestre3 = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],1:2]
         else:
-            trimestre4 = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],1:2]
+            trimestre4 = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[[1,5,9,13,17],1:2]
     df = pd.concat([trimestre1,trimestre2,trimestre3,trimestre4], axis=1)
     df = object_float_injuve(df)
     return df
@@ -40,7 +40,7 @@ def injuve_diez_años(t114,t214,t314,t414,t115,t215,t315,t415,t116,t216,t316,t41
     return injuve_diez_años
 
 def precios_alquiler(archivo):
-    precios_alquiler = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1')
+    precios_alquiler = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1')
     for columna in precios_alquiler.iloc[:,1:]:
         precios_alquiler[columna] = precios_alquiler[columna].str.replace('/m2','').str.replace('%','').str.replace(',','.').astype(float)
     return precios_alquiler
@@ -50,12 +50,12 @@ def distribucion_salarial(dist1,dist2,dist3,dist4,dist5,dist6):
     lista_concat = []
     for archivo in lista_distribucion_salarial:
         if 'mediana' in archivo:
-            distribuion_salarial = (pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1')).iloc[:,3:]
+            distribuion_salarial = (pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1')).iloc[:,3:]
             distribuion_salarial['Total'] = distribuion_salarial['Total'].str.replace('.','').str.replace(',','.').astype(float)
             distribuion_salarial = distribuion_salarial.rename(columns={'Total': archivo})
             lista_concat.append(distribuion_salarial)
         elif 'mediana' not in archivo:
-            distribuion_salarial = (pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1')).iloc[:,3:]
+            distribuion_salarial = (pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1')).iloc[:,3:]
             distribuion_salarial['Total'] = distribuion_salarial['Total'].str.replace('.','').str.replace(',','.').astype(float)
             distribuion_salarial = distribuion_salarial.drop(columns=['Periodo'])
             distribuion_salarial = distribuion_salarial.rename(columns={'Total': archivo})
@@ -64,14 +64,14 @@ def distribucion_salarial(dist1,dist2,dist3,dist4,dist5,dist6):
     return df
 
 def variacion_salario_ipc(archivo):
-    variacion_salario_ipc = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';').iloc[5:,:]
+    variacion_salario_ipc = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';').iloc[5:,:]
     variacion_salario_ipc = (variacion_salario_ipc.drop(columns=['Periodo'])).set_index('Año')
     for columna in variacion_salario_ipc:
         variacion_salario_ipc[columna] = variacion_salario_ipc[columna].str.replace('.','').str.replace(',','.').astype(float)
     return variacion_salario_ipc
 
 def vut(archivo):
-    vut = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1')
+    vut = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1')
     vut = vut.set_index('CIUDADES')
     for columna in vut.iloc[:,:-1]:
         vut[columna] = vut[columna].str.replace(',','').astype(int)
@@ -79,7 +79,7 @@ def vut(archivo):
     return vut
 
 def ipap(archivo):
-    ipap = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1')[::-1]
+    ipap = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1')[::-1]
     ipap = ipap.drop(columns=['Modalidad: Nivel 1','Modalidad: Nivel 2'])
     ipap = ipap.set_index('Periodo')
     for columna in ipap:
@@ -87,7 +87,7 @@ def ipap(archivo):
     return ipap
 
 def alq_madrid(archivo):
-    alq_mad = pd.read_csv(('./data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[2:122,:][::-1]
+    alq_mad = pd.read_csv(('../data/'+ archivo + '.csv'), sep=';', encoding='latin-1').iloc[2:122,:][::-1]
     alq_mad = alq_mad.set_index('Mes')
     for columna in alq_mad:
         alq_mad[columna] = alq_mad[columna].str.replace('/m2','').str.replace('%','').str.replace(',','.').astype(float)
